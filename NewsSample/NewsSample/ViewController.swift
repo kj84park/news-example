f2ff6b4d6f8fad9455078b4289215e495997b39e
kj84park <netom@naver.com>
2018-04-23 23:02:47 +0900

    create workspace.

---
+++ NewsSample/NewsSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsSample
+//
+//  Created by mac on 2018. 4. 23..
+//  Copyright © 2018년 StudioKJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
